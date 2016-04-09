2a77a221baaff2de972b9aca33a3b91be2dce23e
Arctic Ice Studio <development@arcticicestudio.com>
2016-04-09 12:18:42 +0200

    Create JUnit test class file for "com.arcticicestudio.northem.Northem"

---
+++ src/test/java/com/arcticicestudio/northem/NorthemTest.java
@@ -0,0 +1,49 @@
+/*
++++++++++++++++++++++++++++++++++++++++++++
+title     Northem Enumeration JUnit Test  +
+project   northem-java                    +
+file      NorthemTest.java                +
+version                                   +
+author    Arctic Ice Studio               +
+email     development@arcticicestudio.com +
+website   http://arcticicestudio.com      +
+copyright Copyright (C) 2016              +
+created   2016-01-23 10:24 UTC+0100       +
+modified  2016-04-09 12:04 UTC+0200       +
++++++++++++++++++++++++++++++++++++++++++++
+
+[Description]
+JUnit test class for the 'Northem' class of the "Northem - Java" API.
+
+[Copyright]
+Copyright (C) 2016 Arctic Ice Studio <development@arcticicestudio.com>
+
+[References]
+Northem
+  (https://github.com/arcticicestudio/northem)
+Java 8 API Documentation
+  (https://docs.oracle.com/javase/8/docs/api/)
+JavaFX 2.2 API Documentation
+  (https://docs.oracle.com/javafx/2/api/)
+JavaFX 2.2 Oracle Project Website
+  (http://docs.oracle.com/javase/8/javase-clienttechnologies.htm)
+Arctic Versioning Specification
+  (http://specs.arcticicestudio.com/arcver) (ArcVer)
+*/
+
+package com.arcticicestudio.northem;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+/**
+ * JUnit test class for the {@link Northem} class of the
+ * <a href="https://github.com/arcticicestudio/northem-java">Northem - Java</a> API.
+ *
+ * @author Arctic Ice Studio &lt;development@arcticicestudio.com&gt;
+ * @see Northem
+ * @see <a href="http://junit.org/">http://junit.org/</a>
+ * @see <a href="https://github.com/arcticicestudio/northem-java">Northem - Java</a>
+ * @since 0.1.0
+ */
+public class NorthemTest {}
