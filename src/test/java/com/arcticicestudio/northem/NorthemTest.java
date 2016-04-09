9e8bcf23593f6fdc050a4a8be496e58f4f2e47d7
Arctic Ice Studio <development@arcticicestudio.com>
2016-04-09 12:20:00 +0200

    Implement test method "shouldReturnTheCorrectRGBColorCode()"

--- src/test/java/com/arcticicestudio/northem/NorthemTest.java
+++ src/test/java/com/arcticicestudio/northem/NorthemTest.java
@@ -9,7 +9,7 @@ email     development@arcticicestudio.com +
 website   http://arcticicestudio.com      +
 copyright Copyright (C) 2016              +
 created   2016-01-23 10:24 UTC+0100       +
-modified  2016-04-09 12:04 UTC+0200       +
+modified  2016-04-09 12:20 UTC+0200       +
 +++++++++++++++++++++++++++++++++++++++++++
 
 [Description]
@@ -64,5 +64,24 @@ public class NorthemTest {
     assertEquals(Northem.hex(Northem.ORANGE.get()), "#EFA070");
     assertEquals(Northem.hex(Northem.LIME.get()), "#C8CB79");
     assertEquals(Northem.hex(Northem.PURPLE.get()), "#C2A2C9");
+  }
+
+  @Test
+  public void shouldReturnTheCorrectRGBColorCode() {
+    assertEquals(Northem.rgb(Northem.DARKEST_GRAY.get()), "rgb(45, 45, 46)");
+    assertEquals(Northem.rgb(Northem.DARK_GRAY.get()), "rgb(51, 51, 52)");
+    assertEquals(Northem.rgb(Northem.GRAY.get()), "rgb(56, 56, 57)");
+    assertEquals(Northem.rgb(Northem.LIGHT_GRAY.get()), "rgb(96, 96, 98)");
+    assertEquals(Northem.rgb(Northem.DIRTY_WHITE.get()), "rgb(222, 222, 222)");
+    assertEquals(Northem.rgb(Northem.WHITE.get()), "rgb(236, 238, 239)");
+    assertEquals(Northem.rgb(Northem.AQUA.get()), "rgb(144, 205, 199)");
+    assertEquals(Northem.rgb(Northem.LIGHT_BLUE.get()), "rgb(134, 193, 210)");
+    assertEquals(Northem.rgb(Northem.BLUE.get()), "rgb(136, 178, 205)");
+    assertEquals(Northem.rgb(Northem.DARK_BLUE.get()), "rgb(92, 129, 174)");
+    assertEquals(Northem.rgb(Northem.RED.get()), "rgb(223, 117, 116)");
+    assertEquals(Northem.rgb(Northem.YELLOW.get()), "rgb(252, 206, 133)");
+    assertEquals(Northem.rgb(Northem.ORANGE.get()), "rgb(239, 160, 112)");
+    assertEquals(Northem.rgb(Northem.LIME.get()), "rgb(200, 203, 121)");
+    assertEquals(Northem.rgb(Northem.PURPLE.get()), "rgb(194, 162, 201)");
   }
 }
