8a055508cf81d4055f9aec0c39d2db9b54f76272
Arctic Ice Studio <development@arcticicestudio.com>
2016-04-09 12:11:17 +0200

    Create enumeration class file "com.arcticicestudio.northem.Northem"

---
+++ src/main/java/com/arcticicestudio/northem/Northem.java
@@ -0,0 +1,65 @@
+/*
++++++++++++++++++++++++++++++++++++++++++++
+title     Northem                         +
+project   northem-java                    +
+file      Northem.java                    +
+version                                   +
+author    Arctic Ice Studio               +
+email     development@arcticicestudio.com +
+website   http://arcticicestudio.com      +
+copyright Copyright (C) 2016              +
+created   2016-01-20 14:25 UTC+0100       +
+modified  2016-04-09 12:02 UTC+0200       +
++++++++++++++++++++++++++++++++++++++++++++
+
+[Description]
+"Northem" color palette as enumeration for the "Northem - Java" project.
+
+[Copyright]
+Copyright (C) 2016 Arctic Ice Studio <development@arcticicestudio.com>
+
+[References]
+Northem
+  (https://github.com/arcticicestudio/northem)
+Java 8 API Documentation
+  (https://docs.oracle.com/javase/8/docs/api/)
+JavaFX 2.2 API Documentation
+  (https://docs.oracle.com/javafx/2/api/)
+JavaFX 2.2 Oracle Project Website
+  (http://docs.oracle.com/javase/8/javase-clienttechnologies.htm)
+Arctic Versioning Specification
+  (http://specs.arcticicestudio.com/arcver) (ArcVer)
+*/
+package com.arcticicestudio.northem;
+
+/**
+ * <a href="https://github.com/arcticicestudio/northem">Northem</a> color palette
+  * as enumeration of the <a href="https://github.com/arcticicestudio/northem-java">Northem - Java</a> project.
+ * <div>
+ *   <table style="display:inline-block">
+ *     <thead>
+ *       <th style="border:none;width:25px;height:30px;background-color:rgb(45,45,46);margin: 0"></th>
+ *       <th style="border:none;width:25px;height:30px;background-color:rgb(51,51,52);margin: 0"></th>
+ *       <th style="border:none;width:25px;height:30px;background-color:rgb(56,56,57);margin: 0"></th>
+ *       <th style="border:none;width:25px;height:30px;background-color:rgb(96,96,98);margin: 0"></th>
+ *       <th style="border:none;width:25px;height:30px;background-color:rgb(222,222,222);margin: 0"></th>
+ *       <th style="border:none;width:25px;height:30px;background-color:rgb(236,238,239);margin: 0"></th>
+ *       <th style="border:none;width:25px;height:30px;background-color:rgb(144,205,199);margin: 0"></th>
+ *       <th style="border:none;width:25px;height:30px;background-color:rgb(134,193,210);margin: 0"></th>
+ *       <th style="border:none;width:25px;height:30px;background-color:rgb(136,178,205);margin: 0"></th>
+ *       <th style="border:none;width:25px;height:30px;background-color:rgb(92,129,174);margin: 0"></th>
+ *       <th style="border:none;width:25px;height:30px;background-color:rgb(223,117,116);margin: 0"></th>
+ *       <th style="border:none;width:25px;height:30px;background-color:rgb(252,206,133);margin: 0"></th>
+ *       <th style="border:none;width:25px;height:30px;background-color:rgb(239,160,112);margin: 0"></th>
+ *       <th style="border:none;width:25px;height:30px;background-color:rgb(200,203,121);margin: 0"></th>
+ *       <th style="border:none;width:25px;height:30px;background-color:rgb(194,162,201);margin: 0"></th>
+ *     </thead>
+ *   </table>
+ * </div>
+ *
+ * @author Arctic Ice Studio &lt;development@arcticicestudio.com&gt;
+ * @see <a href="https://github.com/arcticicestudio/northem">"Northem" GitHub Repository</a>
+ * @see <a href="https://github.com/arcticicestudio/northem-java">"Northem - Java" GitHub Repository</a>
+ * @since 0.1.0
+ */
+public enum Northem {}
